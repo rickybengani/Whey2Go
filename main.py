700c64f6674f65b278cd1084ed6f6bb0bfac641e
Ricky Bengani <mxb0417@tamu.edu>
2021-04-25 22:50:35 -0500

    All recommendation components working.

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ import math
 app = Flask(__name__)
 app.secret_key = "secret"
 
-train_df = pd.read_csv('./interactions_train.csv')
+# train_df = pd.read_csv('./interactions_train.csv')
 personalized_df = pd.read_csv('./personalized.csv')
 personalized_df[['temp1', 'Recipe', 'temp2']] = personalized_df['Recipe'].str.split("\'", expand=True)
 personalized_df = personalized_df.drop(['temp1', 'temp2'], axis=1)
 # print(personalized_df.head())
+top_recipes_df = pd.read_csv('./top_recipes.csv')
+combined_df = pd.merge(top_recipes_df, personalized_df, how='inner', left_on='RecipeName', right_on='Recipe')
+combined_df = combined_df.drop(['RecipeID', 'RecipeName'], axis=1)
 
 @app.route('/', methods=["POST", "GET"])
 def home():
     # Code to generate non-personalized recipes #
-    non_personalized = ['Pizza', 'Pasta', 'Tacos', 'Burger', 'Naan', 'Queso', 'Shwarma', 'Salad', 'Fried Rice', 'Cake']
+    non_personalized = []
+    for i in range(10):
+        non_personalized.append(combined_df['Recipe'].iloc[i])
+    print(non_personalized)
+    # non_personalized = ['Pizza', 'Pasta', 'Tacos', 'Burger', 'Naan', 'Queso', 'Shwarma', 'Salad', 'Fried Rice', 'Cake']
     # Code to generate non-personalized recipes #
     if request.method == "POST":
         ratings = request.form.getlist("ratings[]")
@@ -35,7 +42,7 @@ def personalized():
         # Code to generate personalized recipes #
         for i in range(len(ratings)):
             if (int(ratings[i]) > 0):
-                ind = personalized_df[personalized_df['Recipe'] == 'apple cider beef stew'].index
+                ind = personalized_df[personalized_df['Recipe'] == non_personalized[i]].index
                 personalized = personalized_df.iloc[ind].to_numpy()
                 personalized = personalized[personalized != math.nan]
                 personalized = personalized[~pd.isnull(personalized)]
